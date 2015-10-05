317395ac8c16e91ca6caf94925e7de8be3ef8963
IstiN (Uladzimir Klyshevich) <istin2007@gmail.com>
2015-10-05 16:08:39 +0300

    added initial project

---
+++ app/src/main/java/com/github/istin/schedule/CoreApplication.java
@@ -0,0 +1,15 @@
+package com.github.istin.schedule;
+
+import android.app.Application;
+
+/**
+ * Created by uladzimir_klyshevich on 10/5/15.
+ */
+public class CoreApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
